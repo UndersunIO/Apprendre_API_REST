023847b782b9e3e78b3a99c0f8528b955d1f56d7
0xUndersun <alex.atc.dev@gmail.com>
2022-05-24 22:42:01 +0200

    [FEATURE] Relier donnee et route express

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Recuperation du paquet express
 const express = require('express');
 
+// import pokemon
+let pokemons = require('./mock-pokemon');
+
 // creation d'une instance d'express
 const app = express();
 // Definition d'un constante comportant le port d'ecoute
@@ -18,11 +21,21 @@ const port = 3000;
 app.get('/', (req, res) => res.send('Hello  express nodemon!'))
 
 
-// Declaration d'un nouveau point de terminaison efficient et dynamique
+// Declaration d'un nouveau point de terminaison efficient et dynamique avec recuperation de parametre de l'url
+/* 
+Etapes :
+- Recuperer l'identifiant du pokemon dans l'url
+-  la recuperer dans le point de terminaison
+- construire une reponse a partir de l'identifiant du pokemon
 
+*/
+// liste de pokemon stocker dans une constante de tableau dans mock-pokemon.js
+// express converti les valeurs en string, utiliser la methode parseInt pour convertir en int et eviter l'erreur typeerror
 app.get('/api/pokemons/:id', (req, res) => {
-    const id = req.params.id;
-    res.send(`Pokemon numero ${id}`)
+    const id = parseInt(req.params.id);
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    res.send(`Pokemon numero ${id} nommée ${pokemon.name}`)
+
 })
 
 // demarre l'api REST sur le port 3000 + message de confirmation
